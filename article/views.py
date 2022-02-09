a742180455286ddcb7048f5e21fdea277d25e219
maksmel488 <melmaks1488@gmail.com>
2022-02-09 11:19:16 +0200

    hm 2 templates

--- article/views.py
+++ article/views.py
@@ -1,26 +1,60 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import random
+import string
 
-def main(request):
-    return HttpResponse("Home Page")
+
+class MyClass:
+    string = ''
+
+    def __init__(self, s):
+        self.string = s
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 def main_acricles(request):
-    return HttpResponse('This is page for acricles')
+    return render(request, 'main_acricles.html')
 
-def acricles_archive(request):
-    return HttpResponse('This is page for acrticles_archive ')
+def first(request):
+    return render(request, 'first.html')
 
 def users(request):
-    return HttpResponse('USERS')
+    return render(request, 'user.html')
 
 def article_number(request, article_id):
-    return HttpResponse(f"This is article_number_id #{article_id}.")
+    num = article_id % 2
+    return render(request, 'article_number.html', {'num': num})
 
 def article_number_archive(request, article_id):
     return HttpResponse(f"This is article_number_id_archive #{article_id}.")
 
-def article_number_text(request, article_id, slug_text=''):
-    return HttpResponse(  "Arcticle ID is: {}. {}".format(article_id, "Article name is: {}".format(slug_text)
-        if slug_text else "This is unnamed article!!!"))
+def article_number_slug(request, article_id, slug_text=''):
+    num = article_id % 2
+    text = slug_text
+
+    return render(request, 'article_number_slug.html', {'num': num, 'text': text})
+
 
 def users_number(request, users_id):
     return HttpResponse(f"This is users_number_id #{users_id}.")
+
+def random_page(request):
+    pages = ['first.html', 'article_number.html', 'article_number_slug.html', 'user.html']
+    rnd_page = pages[random.randint(0, 3)]
+    return render(request, rnd_page)
+
+
+def random_slug(request):
+    letters = string.ascii_lowercase
+    pages = ['article_number.html', 'article_number_slug', 'user.html']
+    rnd_page = pages[random.randint(0, 2)]
+    rnd_slug = ''.join(random.choice(letters) for i in range(random.randint(5,10)))
+    return render(request, rnd_page, {
+        'text': rnd_slug})
+
+
+
+
--- vector/urls.py
+++ vector/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from article.views import main, main_acricles, users_number, acricles_archive, users, article_number, article_number_archive, article_number_text
+from article.views import index, first,  main_acricles, users_number,  users, article_number, article_number_archive, article_number_slug, random_slug, random_page
 
 
 
 urlpatterns = [
-    path('', main),
-    path('acricles/', main_acricles, name='mail_acricles'),
-    path('acrticles/archive', acricles_archive, name='mail_acrticles'),
+    path('', index, name='main'),
+    path('first', first, name='first'),
+    path('acricles/', main_acricles, name='main_acricles'),
     path('users/', users, name='users'),
     path('article/<int:article_id>/', article_number, name='article_number'),
     path('article/<int:article_id>/archive', article_number_archive, name='article_number'),
-    path('article/<int:article_id>/<slug:slug_text>', article_number_text, name='article_number_text'),
-    path('users/<int:users_id>/', users_number, name='users_number')
+    path('article/<int:article_id>/<slug:slug_text>', article_number_slug, name='article_number_slug'),
+    path('users/<int:users_id>/', users_number, name='users_number'),
+    path('random_page/', random_page, name='random_page'),
+    path('random_slug/', random_slug, name='random_slug'),
 
 
 ]
